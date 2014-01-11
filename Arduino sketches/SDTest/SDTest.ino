5e3058a1ee871965f97be4481df02f9bada319b5
Claudio Indellicati <bitron.it@gmail.com>
2014-01-11 13:56:33 +0100

    Modified 'SDTest.ino'.

--- Arduino sketches/SDTest/SDTest.ino
+++ Arduino sketches/SDTest/SDTest.ino
@@ -10,7 +10,7 @@ ArduinoOutStream cout(Serial);
 #define error(s) sd.errorHalt_P(PSTR(s))
 
 int ggmm = 1;
-int dat = 0;
+
 
 void setup() {
 
@@ -34,60 +34,49 @@ void loop() {
   }
   delay(400);  // catch Due reset problem
 
-
-  //compongo una data
-  String date ="0";
-  date +=ggmm;
-  date +=ggmm;
-  date +="0";
-  ggmm++;
-  if(ggmm ==10) ggmm=1;
+  //creo la data
+  String date = createDate();
   Serial.print ("data   :"); 
   Serial.println (date); 
 
-
-  //costruisco la mia stringa di dati
-  if (dat == 10) dat = 0;
-  String dataString = " LightLog,";
-  dataString += dat;
-  dataString += " SoilHumidity,";
-  dataString += dat*10;
-  dataString += " Temperature, ";
-  dataString += dat*100;
+  //creo i dati
+  String dataString = createData();
   Serial.print ("dataString   :"); 
   Serial.println (dataString); 
-  dat ++;
+  delay(1000);
 
 
   //creo il nome del file per scrivere dentro i dati:solo per ACQ 
-  String nameoffile;
-  nameoffile = "/ACQ/"+date+".txt";
+  char* ACQ="ACQ";
+  char* RTS="RTS";
   int lettersnumber=14; //lunghezza del path
-  char nameoffile_C[lettersnumber];
-  nameoffile.toCharArray(nameoffile_C, lettersnumber);
-
-
+  char* nameoffile_C[lettersnumber];
+  *nameoffile_C=createNameoffile(date, ACQ );
+  
   //controllo il nome del file appena creato  
   Serial.println ("--...---"); 
-  Serial.println (nameoffile_C);
-
-
+  Serial.println (*nameoffile_C);
+  
   //apro il file in ACQ
-  SdFile file(nameoffile_C , O_RDWR | O_APPEND | O_CREAT);
+  SdFile file(*nameoffile_C , O_RDWR | O_APPEND | O_CREAT);
   sd.ls(LS_R);  
   //scrivo all'interno del file aperto
   if (!file.isOpen()) error("non ho creato il file contenente i dati");
   file.print(dataString);
 
   //creo il nome del file per rinominarlo in RTS 
-  nameoffile = "/RTS/"+date+".txt";
-  //nameoffile = +date+".txt";
-  nameoffile.toCharArray(nameoffile_C, lettersnumber);
+  *nameoffile_C=createNameoffile(date, RTS );
   Serial.print ("creato il nuovo nomefile : ");
-  Serial.println (nameoffile_C);
-  int escape = false;   //to exit from the while where rename in another directory
+  Serial.println (*nameoffile_C);
+  //file.close();
+  
+  //to exit from the while where rename in another directory
+  int escape = false;  
   while (!escape) {
-    if(file.rename(sd.vwd(), nameoffile_C)){
+    Serial.print (*nameoffile_C);
+    Serial.println ("<");
+    Serial.print (sd.vwd());
+    if(file.rename(sd.vwd(), *nameoffile_C)){
       escape = true;
       Serial.println ("lo ho rimoninato in RTS  ");  // se esco qui ho rinominato il file senza trovare in RTS lo stesso nome
     }  
@@ -100,11 +89,11 @@ void loop() {
   sd.ls(LS_R); 
 
 
-
   //mi sposto in RTS
   if (!sd.chdir("RTS/")) {
     sd.errorHalt("chdir failed. Did you run eventlog.pde?");
   }
+
 
   //leggo tutti i file in RTS, immagazzino i nomi in fileRTS[] e li sposto
   //char *fileRTS;
@@ -119,51 +108,27 @@ void loop() {
   Serial.print("ho letto :");
   delay(1000);
   Serial.println(sfileRTS);
+  Serial.println(fileRTS[6]);
   //file.close();
-
-
-
-
-  //da qui entro in lettura. leggo i dati e poi dovrò spedire storetoSNT
-  //  int c;
-  //  String fname;
-  //  String storetoSND;   
-  // // SdFile file1;
-  //  ifstream file1(fileRTS);
-  //  if (!file1.is_open()) sd.errorHalt("open failed in RTS");
-  //
-  //  while ((c = file.get()) >= 0) {
-  //    storetoSND+= (char)c;
-  //  }
-
 
   int c;
   String sfname;
-  char fname[70];
+
 
   // String storetoSND;
   SdFile myFile;
   if (!myFile.open(fileRTS, O_READ)) {
     sd.errorHalt("opening test.txt for read failed");
   }
-  //while ((c = myFile.read()) >= 0) Serial.write(c);
-  //  sd.ls(LS_R);
-
-
 
   while ((c = myFile.read()) >= 0) {
-  //char line[70];
-  //while ((c = myFile.fgets(line, sizeof(line))) >= 0){ 
-   // Serial.write(c); 
+    //Serial.write(c); 
     sfname += char(c);
     //file.close();
   }
-  //sfname.toCharArray(fname, 70); 
+
   Serial.print("i dati ->");
-  //  Serial.println(storetoSND);
-  //  Serial.print("letti dal file");
   Serial.println(sfname);
-  //  Serial.write(c);
   myFile.close();
   sd.ls(LS_R);
 
@@ -171,8 +136,67 @@ void loop() {
 }
 
 
+String createDate()
+{
+  //compongo una data
+  String date ="0";
+  date +=ggmm;
+  date +=ggmm;
+  date +="0";
+  ggmm++;
+  if(ggmm ==10) ggmm=1;
+  return date;
+}
 
+String createData(){
+  //costruisco la mia stringa di dati
+  int dat = 0;
+  if (dat == 10) dat = 0;
+  String dataString = " LightLog,";
+  dataString += dat;
+  dataString += " SoilHumidity,";
+  dataString += dat*10;
+  dataString += " Temperature, ";
+  dataString += dat*100;
+  dat ++;
+  return dataString;
+}
 
+char* createNameoffile(String date,  char* DIR)
+{
+  String nameoffile;
+  //Serial.println(DIR);
+  nameoffile = "/"+ String(DIR) +"/"+date+".txt";
+  //Serial.println (nameoffile);
+  int lettersnumber=14; //lunghezza del path
+  char nameoffile_C[lettersnumber];
+  nameoffile.toCharArray(nameoffile_C, lettersnumber);
+  //Serial.println (nameoffile_C);
+  return nameoffile_C;
+}
 
-
-
+/*void opentoWrite(char* nameoffile[], String dataString)
+{
+  Serial.println (*nameoffile);
+  SdFile file(*nameoffile , O_RDWR | O_APPEND | O_CREAT);
+  sd.ls(LS_R);  
+  //scrivo all'interno del file aperto
+  if (!file.isOpen()) error("non ho creato il file contenente i dati");
+  file.print(dataString);
+}*/
+/*void reName(char* nameoffile_C)
+{
+  int escape = false;   //to exit from the while where rename in another directory
+  while (!escape) {
+    if(file.rename(sd.vwd(), *nameoffile_C)){
+      escape = true;
+      Serial.println ("lo ho rimoninato in RTS  ");  // se esco qui ho rinominato il file senza trovare in RTS lo stesso nome
+    }  
+    else {
+      Serial.println ("Error nel rinominarlo ");
+      escape = true;                //togliere<<<<--------------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+  }
+  file.close();
+  sd.ls(LS_R); 
+}*/
