4fd1d385f6055a66cb927b90e9f3ae5bd20c9c3e
Claudio Indellicati <bitron.it@gmail.com>
2014-01-11 13:54:38 +0100

    Fixed previous code.

--- Arduino sketches/SDTest/SDTest.ino
+++ Arduino sketches/SDTest/SDTest.ino
@@ -1,5 +1,5 @@
-
 #include <SdFat.h>
+#include <SdFatUtil.h>
 // SD chip select pin
 const uint8_t chipSelect = SS;
 // file system
@@ -8,112 +8,171 @@ SdFat sd;
 ArduinoOutStream cout(Serial);
 // store error strings in flash to save RAM
 #define error(s) sd.errorHalt_P(PSTR(s))
-//------------------------------------------------------------------------------
 
- int i = 0;
- int n = 0;
- int connection;
-  
-  
+int ggmm = 1;
+int dat = 0;
+
 void setup() {
+
   Serial.begin(9600);
-  while (!Serial) {}  // wait for Leonardo
-  
+  while (!Serial) {
+  }  // wait for Leonardo
   // initialize the SD card at SPI_HALF_SPEED to avoid bus errors with
   // breadboards.  use SPI_FULL_SPEED for better performance.
   if (!sd.begin(chipSelect, SPI_HALF_SPEED)) sd.initErrorHalt();
-  
-  /******************************__.
-  creo le 3 directory 
-  */
+
   sd.mkdir("ACQ"); //directory dovre andranno i dati ACQuisiti ancora da spedire
   sd.mkdir("RTS"); //directory dovre andranno i dati acquisiti Ready To Send
-  sd.mkdir("RTS"); //directory dovre andranno i dati acquisiti spediti SeNT
- 
+  sd.mkdir("SNT"); //directory dovre andranno i dati acquisiti spediti SeNT
+
 }
+
 void loop() {
- 
-  // ****metto un blocco  pratico
+
   cout << pstr(" Type any character to start.") << endl;
-  while (Serial.read() <= 0) {}
+  while (Serial.read() <= 0) {
+  }
   delay(400);  // catch Due reset problem
 
-  if (i == 10) i = 0;     //ogni dieci faccio un ciclo
-  
+
+  //compongo una data
+  String date ="0";
+  date +=ggmm;
+  date +=ggmm;
+  date +="0";
+  ggmm++;
+  if(ggmm ==10) ggmm=1;
+  Serial.print ("data   :"); 
+  Serial.println (date); 
+
+
   //costruisco la mia stringa di dati
-  String dataString = "LightLog,";
-  dataString += i;
-  dataString += "\nSoilHumidity,";
-  dataString += i*10;
-  dataString += "\nTemperature, ";
-  dataString += i*100;
-  
-  Serial.println ("fatto il calcolo : ");
+  if (dat == 10) dat = 0;
+  String dataString = " LightLog,";
+  dataString += dat;
+  dataString += " SoilHumidity,";
+  dataString += dat*10;
+  dataString += " Temperature, ";
+  dataString += dat*100;
+  Serial.print ("dataString   :"); 
   Serial.println (dataString); 
- 
-  /* creo il primo file nella cartella di acquisizione, al prossimo
-  giro di loop questa cartella sarà vuota perchè il file da me creato e spostano nella dir RTS*/ 
-  SdFile file( "ACQ/a.txt", O_WRITE | O_CREAT);
-  if (!file.isOpen()) error("a");
-  file.print(dataString);                              //contollo
-  file.print("sono in ACQ/a.txt");                     //contollo
-  cout << pstr("sono in ACQ/a.txt") << endl;
-  sd.ls(LS_R);                                         //guardo cosa c'e nella mia SD
-  //file.close();
-  
-  // se ho già a.txt nella cartella RTS allora lo cancello altrimenti avrò errore dopo quando lo muovo qui
-  if(file.remove(sd.vwd(), "RTS/a.txt")){
-    Serial.print("c'era qualcosa in RTS ma lo ho tolto "); 
-    cout << pstr("vediamo le cartelle") << endl;
-  sd.ls(LS_R);                                         //guardando il seriale qui dovrei vedere ls -r di SD e RTS dovrebbe essere vuota
+  dat ++;
+
+
+  //creo il nome del file per scrivere dentro i dati:solo per ACQ 
+  String nameoffile;
+  nameoffile = "/ACQ/"+date+".txt";
+  int lettersnumber=14; //lunghezza del path
+  char nameoffile_C[lettersnumber];
+  nameoffile.toCharArray(nameoffile_C, lettersnumber);
+
+
+  //controllo il nome del file appena creato  
+  Serial.println ("--...---"); 
+  Serial.println (nameoffile_C);
+
+
+  //apro il file in ACQ
+  SdFile file(nameoffile_C , O_RDWR | O_APPEND | O_CREAT);
+  sd.ls(LS_R);  
+  //scrivo all'interno del file aperto
+  if (!file.isOpen()) error("non ho creato il file contenente i dati");
+  file.print(dataString);
+
+  //creo il nome del file per rinominarlo in RTS 
+  nameoffile = "/RTS/"+date+".txt";
+  //nameoffile = +date+".txt";
+  nameoffile.toCharArray(nameoffile_C, lettersnumber);
+  Serial.print ("creato il nuovo nomefile : ");
+  Serial.println (nameoffile_C);
+  int escape = false;   //to exit from the while where rename in another directory
+  while (!escape) {
+    if(file.rename(sd.vwd(), nameoffile_C)){
+      escape = true;
+      Serial.println ("lo ho rimoninato in RTS  ");  // se esco qui ho rinominato il file senza trovare in RTS lo stesso nome
+    }  
+    else {
+      Serial.println ("Error nel rinominarlo ");
+      escape = true;                //togliere<<<<--------------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
   }
-  
-  //sposto il file a.txt da ACQ in RTS
-  if (!file.rename(sd.vwd(), "RTS/a.txt")) error("a");     
-  file.println("sono in RTS/a.txt");
-
-  // list files
-  cout << pstr("sono in RTS/a.txt") << endl;
-  sd.ls(LS_R);
-  
-  Serial.print("ho già chiuso, escluso questo ");
-  Serial.print(n);
-  Serial.println(" loop ");
-
-
-  /*cout << pstr("type 1 or right connection and trasmission ofn content, any other key for wrong ones") << endl;
-  while (connection <= 0) {
-  connection = Serial.read();
-  Serial.println(connection);
-  
-  Serial.println(connection, DEC);
- // while (connection <= 0) {
-  if (connection == 1)  {
-    Serial.print(" premuto uno e si vede qui ");
-    Serial.print(connection, DEC);
-      }
-  }*/
-  delay(400);  // catch Due reset problem
-
-  
-
-  // close file before rename(oldPath, newPath)
   file.close();
-/*
-  // move DIR1 into DIR2 and rename it DIR3
-  if (!sd.rename("DIR1", "DIR2/DIR3")) error("DIR2/DIR3");
+  sd.ls(LS_R); 
 
-  // open file for append in new location and add a line
-  if (!file.open("DIR2/DIR3/NAME3.TXT", O_WRITE | O_APPEND)) {
-    error("DIR2/DIR3/NAME3.TXT");
+
+
+  //mi sposto in RTS
+  if (!sd.chdir("RTS/")) {
+    sd.errorHalt("chdir failed. Did you run eventlog.pde?");
   }
-  file.println("A line for DIR2/DIR3/NAME3.TXT");
 
-  // list files
-  cout << pstr("------") << endl;
+  //leggo tutti i file in RTS, immagazzino i nomi in fileRTS[] e li sposto
+  //char *fileRTS;
+  char fileRTS[10];
+  String sfileRTS;
+  while (file.openNext(sd.vwd(), O_READ)) {
+    //while (file.openNext(*RTS, O_READ)) {  
+    file.getFilename(fileRTS);
+    file.close();
+    sfileRTS=String(fileRTS);
+  }
+  Serial.print("ho letto :");
+  delay(1000);
+  Serial.println(sfileRTS);
+  //file.close();
+
+
+
+
+  //da qui entro in lettura. leggo i dati e poi dovrò spedire storetoSNT
+  //  int c;
+  //  String fname;
+  //  String storetoSND;   
+  // // SdFile file1;
+  //  ifstream file1(fileRTS);
+  //  if (!file1.is_open()) sd.errorHalt("open failed in RTS");
+  //
+  //  while ((c = file.get()) >= 0) {
+  //    storetoSND+= (char)c;
+  //  }
+
+
+  int c;
+  String sfname;
+  char fname[70];
+
+  // String storetoSND;
+  SdFile myFile;
+  if (!myFile.open(fileRTS, O_READ)) {
+    sd.errorHalt("opening test.txt for read failed");
+  }
+  //while ((c = myFile.read()) >= 0) Serial.write(c);
+  //  sd.ls(LS_R);
+
+
+
+  while ((c = myFile.read()) >= 0) {
+  //char line[70];
+  //while ((c = myFile.fgets(line, sizeof(line))) >= 0){ 
+   // Serial.write(c); 
+    sfname += char(c);
+    //file.close();
+  }
+  //sfname.toCharArray(fname, 70); 
+  Serial.print("i dati ->");
+  //  Serial.println(storetoSND);
+  //  Serial.print("letti dal file");
+  Serial.println(sfname);
+  //  Serial.write(c);
+  myFile.close();
   sd.ls(LS_R);
-*/
-  i++;
-  n++;
-  cout << pstr("Done") << endl;
+
+  Serial.println ("done"); 
 }
+
+
+
+
+
+
+
